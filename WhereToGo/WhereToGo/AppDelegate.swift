19e8eda9e09d6d48c421c4fe0d84e632ef00febd
jean.santos <jean.santos@bancointer.com.br>
2020-12-23 12:32:36 -0300

    Started and configured  the project. Included file Podfile with Alamofire lib.

---
+++ WhereToGo/WhereToGo/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  WhereToGo
+//
+//  Created by Jean Carlos Antônio Pereira dos Santos on 23/12/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootViewController = ViewController()
+        let navigation = UINavigationController(rootViewController: rootViewController)
+        
+        window?.rootViewController = navigation
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ WhereToGo/WhereToGo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WhereToGo
+//
+//  Created by Jean Carlos Antônio Pereira dos Santos on 23/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
